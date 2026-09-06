9da35720423cbc5a66086d0832cb98934c168019
Bơ <101487639+drphuongtnm@users.noreply.github.com>
2026-09-06 11:02:01 +0700

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const closeToOpen = {
+            '}': '{',
+            ']': '[',
+            ')': '('
+        }
+        const stack = [];
+        for(let c of s){
+            if(closeToOpen[c]){
+                if(stack.length > 0 && closeToOpen[c] === stack[stack.length-1]){
+                    stack.pop();
+                }else{
+                    return false
+                }
+            }else{
+                stack.push(c);
+            }
+        }
+        return stack.length === 0;
+    }
+}
