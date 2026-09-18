c4f6bd9ebfd9b3dbafb9ddd8e4c8d36bdc0e2abd
Bơ <101487639+drphuongtnm@users.noreply.github.com>
2026-09-18 21:38:52 +0700

    Add: serialize-and-deserialize-binary-tree - submission-4

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-4.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Codec {
+    /**
+     * Encodes a tree to a single string.
+     *
+     * @param {TreeNode} root
+     * @return {string}
+     */
+    serialize(root) {
+        const res = [];
+        this.dfs(root, res);
+        return res.join(',')
+    }
+    dfs(root, res){
+        if(root === null){
+            res.push('N');
+            return null;
+        }
+        res.push(root.val.toString());
+        this.dfs(root.left, res);
+        this.dfs(root.right,res);
+    }
+
+    /**
+     * Decodes your encoded data to tree.
+     *
+     * @param {string} data
+     * @return {TreeNode}
+     */
+    deserialize(data) {
+        const i = { val: 0};
+        const vals = data.split(',');
+        return this.dfsSerialize(vals, i);
+    }
+
+    dfsSerialize(vals, i) {
+        if(vals[i.val] === 'N'){
+            i.val++;
+            return null;
+        }
+        const node = new TreeNode(parseInt(vals[i.val]));
+        i.val++;
+        node.left = this.dfsSerialize(vals, i);
+        node.right = this.dfsSerialize(vals, i)
+        return node;
+    }
+}
