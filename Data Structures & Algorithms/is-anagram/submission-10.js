466b69dd11d6e1fc8b21b7e3a2567b4b3a12a7e2
Bơ <101487639+drphuongtnm@users.noreply.github.com>
2026-09-23 21:33:12 +0700

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        let sSort = s.split('').sort().join();
+        let tSort = t.split('').sort().join();
+        return sSort === tSort
+    }
+}
